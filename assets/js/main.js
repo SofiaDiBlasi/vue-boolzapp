83548ba50941d0e59f1b229701dce041f8074cab
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-05-15 23:34:46 +0100

    modificato contenuto dell'array

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,157 +12,157 @@ const {
         
         contacts: [
             {
-                name: 'Michele',
+                name: 'Sauron',
                 avatar: './assets/img/avatar_1.jpg',
                 visible: true,
                 messages: [
                     {
                         date: '10/01/2020 15:30:55',
-                        message: 'Hai portato a spasso il cane?',
+                        message: 'Il piano procede bene...',
                         status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
-                        message: 'Ricordati di stendere i panni',
+                        message: '...dopo devo incontrarmi con Gandalf',
                         status: 'sent'
                     },
                     {
                         date: '10/01/2020 16:15:22',
-                        message: 'Tutto fatto!',
+                        message: 'Va bene, mi raccomando...fingiti interessato !',
                         status: 'received'
                     }
                 ],
             },
             {
-                name: 'Fabio',
+                name: 'Gandalf',
                 avatar: './assets/img/avatar_2.jpg',
                 visible: true,
                 messages: [
                     {
                         date: '20/03/2020 16:30:00',
-                        message: 'Ciao come stai?',
+                        message: 'Quando Arrivi?',
                         status: 'sent'
                     },
                     {
                         date: '20/03/2020 16:30:55',
-                        message: 'Bene grazie! Stasera ci vediamo?',
+                        message: 'Il cavallo sta finendo di bere al fiume, 5 minuti e dovrei esserci.',
                         status: 'received'
                     },
                     {
                         date: '20/03/2020 16:35:00',
-                        message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        message: 'Ok, ho preparato una sorpresa per te !',
                         status: 'sent'
                     }
                 ],
             },
             {
-                name: 'Samuele',
+                name: 'Capo Uruk-hai',
                 avatar: './assets/img/avatar_3.jpg',
                 visible: true,
                 messages: [
                     {
                         date: '28/03/2020 10:10:40',
-                        message: 'La Marianna va in campagna',
+                        message: 'Abbiamo Merry e Pipino !',
                         status: 'received'
                     },
                     {
                         date: '28/03/2020 10:20:10',
-                        message: 'Sicuro di non aver sbagliato chat?',
+                        message: 'Ottimo lavoro',
                         status: 'sent'
                     },
                     {
                         date: '28/03/2020 16:15:22',
-                        message: 'Ah scusa!',
+                        message: 'Ora possiamo andare a fare merenda?',
                         status: 'received'
                     }
                 ],
             },
             {
-                name: 'Alessandro B.',
+                name: 'Goblin',
                 avatar: './assets/img/avatar_4.jpg',
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
-                        message: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        date: '10/01/2020 15:50:00',
+                        message: 'Te lo avrà già chiesto il capo, ma possiamo andare a fare merenda?',
+                        status: 'received'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
-                        message: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        date: '10/01/2020 15:30:55',
+                        message: 'Non ho il tuo numero salvato, chi sei?',
+                        status: 'sent'
                     }
                 ],
             },
             {
-                name: 'Alessandro L.',
+                name: 'Barbalbero',
                 avatar: './assets/img/avatar_5.jpg',
                 visible: true,
                 messages: [
                     {
                         date: '10/01/2020 15:30:55',
-                        message: 'Ricordati di chiamare la nonna',
+                        message: 'Potrei aver accidentalmente devastato parte della foresta...',
                         status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
-                        message: 'Va bene, stasera la sento',
+                        message: 'Non mi resta che vedermela con Isengard stanotte, con sassi e pietre.',
                         status: 'received'
                     }
                 ],
             },
             {
-                name: 'Claudia',
+                name: 'Palantir',
                 avatar: './assets/img/avatar_6.jpg',
                 visible: true,
                 messages: [
                     {
                         date: '10/01/2020 15:30:55',
-                        message: 'Ciao Claudia, hai novità?',
+                        message: 'Palantir, che tempo fa a Barad-dûr?',
                         status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
-                        message: 'Non ancora',
+                        message: 'A Barad-dûr, Terra di Mezzo, il cielo sarà nuvoloso con umidità del 30%.',
                         status: 'received'
                     },
                     {
                         date: '10/01/2020 15:51:00',
-                        message: 'Nessuna nuova, buona nuova',
+                        message: 'Palantir, imposta una sveglia "chiamata giornaliera con Sauron" alle 17 del pomeriggio',
                         status: 'sent'
                     }
                 ],
             },
             {
-                name: 'Federico',
+                name: 'Radagast',
                 avatar: './assets/img/avatar_7.jpg',
                 visible: true,
                 messages: [
                     {
                         date: '10/01/2020 15:30:55',
-                        message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                        message: 'Ho visto delle aquile vicino la mia torre...ne sai qualcosa?',
                         status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
-                        message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                        message: 'No...*bombastic side eye*',
                         status: 'received'
                     }
                 ],
             },
             {
-                name: 'Davide',
+                name: 'Mamma',
                 avatar: './assets/img/avatar_8.jpg',
                 visible: true,
                 messages: [
                     {
                         date: '10/01/2020 15:30:55',
-                        message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                        message: 'Ciao, hai mangiato oggi?',
                         status: 'received'
                     },
                     {
                         date: '10/01/2020 15:50:00',
-                        message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                        message: 'No, sto ancora lavorando, lo faccio più tardi!',
                         status: 'sent'
                     },
                     {
